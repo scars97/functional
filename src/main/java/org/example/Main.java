61d5af395396d0d3dd824f596248f9f32a895c8e
박성현 <qkr970504@gmail.com>
2024-04-29 22:52:50 +0900

    함수형 개념 - 순수함수와 참조투명성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,22 @@
 package org.example;
 
+import java.time.LocalTime;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String s = buildGreeting("성현");
+        System.out.println(s);
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static String buildGreeting(String name) {
+        var now = LocalTime.now();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if (now.getHour() < 12) {
+            return "Good morning " + name;
+        } else {
+            return "Hello " + name;
         }
     }
 }
