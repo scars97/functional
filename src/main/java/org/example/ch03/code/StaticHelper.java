f11e297fc787952f8f13fc8e7b367c619dd566f3
박성현 <qkr970504@gmail.com>
2024-05-15 21:02:02 +0900

    정적 헬퍼

---
+++ src/main/java/org/example/ch03/code/StaticHelper.java
@@ -0,0 +1,18 @@
+package org.example.ch03.code;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class StaticHelper {
+
+    public static void main(String[] args) {
+        Function<String, String> removeLowerCaseA = str -> str.replace("a", "");
+        Function<String, String> upperCase = String::toUpperCase;
+
+        Function<String, String> stringOperations =
+                removeLowerCaseA.andThen(upperCase);
+
+        String apply = stringOperations.apply("abcd");
+        System.out.println(apply);
+    }
+}
