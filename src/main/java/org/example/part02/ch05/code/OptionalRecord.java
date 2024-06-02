70fa86f8a4ca6f64031466b304f5535361098f79
박성현 <qkr970504@gmail.com>
2024-06-02 10:53:10 +0900

    레코드 Optional 정리2

--- src/main/java/org/example/part02/ch05/code/OptionalRecord.java
+++ src/main/java/org/example/part02/ch05/code/OptionalRecord.java
@@ -1,24 +1,27 @@
 package org.example.part02.ch05.code;
 
-import java.util.Objects;
 import java.util.Optional;
 
 public class OptionalRecord {
 
     public static void main(String[] args) {
-        User user = new User("userId", "성현", null);
+        String email = "qktjd97@naver.com";
+
+        String test = email == "qkr970504" ? email : null;
+
+        User user = new User("userId", "성현", test);
         System.out.println(user.email);
 
     }
 
     public record User(String id, String name, Optional<String> email) {
 
-        public User {
+        /*public User {
             Objects.requireNonNull(email, "Optional<String> group must not be null");
-        }
-
-        /*public User(String id, String name, String email) {
-            this(id, name, Optional.ofNullable(email));
         }*/
+
+        public User(String id, String name, String email) {
+            this(id, name, Optional.ofNullable(email));
+        }
     }
 }
