5e59a93c4662144c1eeb269bf080d5b2ff4e118f
park <qkr970504@gmail.com>
2024-06-18 12:50:44 +0900

    병렬 스트림 - ForkJoinPool

--- src/main/java/org/example/part02/ch08/code/ParallelEx.java
+++ src/main/java/org/example/part02/ch08/code/ParallelEx.java
@@ -1,7 +1,9 @@
 package org.example.part02.ch08.code;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ParallelEx {
 
@@ -12,10 +14,10 @@ public class ParallelEx {
         }
 
         // 단일 스레드 처리 시간 측정
-        long startTime = System.nanoTime();
-        /*int[] singleThreadMultiple1 = numbers.stream()
+        /*long startTime = System.nanoTime();
+        *//*int[] singleThreadMultiple1 = numbers.stream()
                 .mapToInt(n -> n * n)
-                .toArray();*/
+                .toArray();*//*
         Object[] singleThreadMultiple2 = numbers.stream()
                 .map(n -> n * n)
                 .toArray();
@@ -25,9 +27,9 @@ public class ParallelEx {
 
         // 병렬 스트림 처리 시간 측정
         startTime = System.nanoTime();
-        /*int[] multiThreadMultiple1 = numbers.parallelStream()
+        *//*int[] multiThreadMultiple1 = numbers.parallelStream()
                 .mapToInt(n -> n * n)
-                .toArray();*/
+                .toArray();*//*
         Object[] multiThreadMultiple2 = numbers.parallelStream()
                 .map(n -> n * n)
                 .toArray();
@@ -39,7 +41,20 @@ public class ParallelEx {
             System.out.println("단일 승");
         } else {
             System.out.println("멀티 승");
-        }
-        
+        }*/
+
+
+        long startTime = System.nanoTime();
+        List<Integer> result = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * n)
+                .parallel() // 병렬 모드 전환
+                .collect(Collectors.toList());
+        long threadTime = System.nanoTime() - startTime;
+        System.out.println("처리 시간: " + threadTime / 1_000_000.0 + " ms");
+
+        // 결과 출력
+        //System.out.println(result);
+        // [4, 16, 36, 64, 100]
     }
 }
