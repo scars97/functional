83e1574fc07442c826e0d891cae6c6897374d258
박성현 <qkr970504@gmail.com>
2024-07-02 21:57:56 +0900

    잘못된 예외 처리 문제2 & 좋은 예외 처리 ~ing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,35 @@
 package org.example;
 
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
-        HelloWorld helloWorld = new HelloWorld() {
-            @Override
-            public String sayHello(String name) {
-                return "hello, " + name + "!";
-            }
-        };
-
-        System.out.println(helloWorld.sayHello("성현"));
-
-        HelloWorld helloWorldLambda = name -> "hello, " + name + "!";
-
-        System.out.println(helloWorldLambda.sayHello("성현2"));
-
+        try {
+            methodA();
+        } catch (MyException e) {
+            System.out.println("예외 처리: " + e.getMessage());
+        } finally {
+            System.out.println("finally 블록 실행");
+        }
+        System.out.println("프로그램 종료");
     }
 
-    interface HelloWorld {
-        String sayHello(String name);
+    public static void methodA() throws MyException {
+        System.out.println("methodA 시작");
+        methodB();
+        System.out.println("methodA 종료");
     }
+
+    public static void methodB() throws MyException {
+        System.out.println("methodB 시작");
+        throw new MyException("문제가 발생했습니다!"); // 예외 발생
+    }
+
+    static class MyException extends Exception {
+        public MyException(String message) {
+            super(message);
+        }
+    }
+
 }
